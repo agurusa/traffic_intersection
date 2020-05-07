7040045ac9bdfca7802b1a65aeee2ba26c82b60f
Aarthi Gurusami <aarthi.gurusami@gmail.com>
2020-05-07 14:45:05 -0700

    add test to verify early exit of traffic lanes

--- controller/tests/test_controller.py
+++ controller/tests/test_controller.py
@@ -1,6 +1,9 @@
 import pytest
 from controller import consts
 from controller.controller import Controller
+import threading
+import datetime as dt
+from time import sleep
 
 
 @pytest.fixture
@@ -31,10 +34,23 @@ def test_waiting(control):
     assert control.waiting() is True
 
 
-# def test_run(control):
-#     control.timer.time = 55  # test 5 seconds
-#     _lanes = control._lanes
-#     for lane in _lanes:  # put one car into each lane
-#         lane.cars.put(1)
-#
-
+def test_run(control):
+    for lane in control._lanes:  # put one car into each lane
+        lane.cars.put(1)
+        
+    now = dt.datetime.now()
+    end = now + dt.timedelta(0, 5)  # test for 5 seconds
+    green_lanes = set()
+    lock = threading.Lock()
+    intersection = threading.Thread(target=control.run, daemon=True)
+    intersection.start()
+    while now < end:
+        # move traffic
+        with lock:
+            if not control.current_lane.cars.empty():
+                control.current_lane.cars.get()
+            _current_lane = control.current_lane
+            sleep(0.25)
+        green_lanes.add(_current_lane.direction)
+        now = dt.datetime.now()
+    assert green_lanes == set(consts.ORDER)
